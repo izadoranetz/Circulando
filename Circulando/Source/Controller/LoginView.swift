d996ca6ca7dc430a9d5840c53259436ef8d49107
Izadora Netz <izadora.ns@gmail.com>
2022-11-28 17:59:53 -0300

    image label

--- Circulando/Source/Controller/LoginView.swift
+++ Circulando/Source/Controller/LoginView.swift
@@ -17,6 +17,7 @@ class LoginView: UIView {
         setupVisualElements()
     }
 
+    //imagem
     var imageLogin: UIImageView = {
         let imagem = UIImageView ()
         imagem.image = UIImage(named: "ImageLogin")
@@ -26,8 +27,21 @@ class LoginView: UIView {
         return imagem
     }()
 
+    //label
+    var imageLabel: UILabel = {
+        let label = UILabel ()
+        label.textColor = UIColor(red: 0.541, green: 0.541, blue: 0.557, alpha: 1)
+        label.font = UIFont(name: "SFProDisplay-Light", size: 16)
+        label.textAlignment = .center
+        label.attributedText = NSMutableAttributedString(string: "Aumente o ciclo de vida de suas coisas", attributes: [NSAttributedString.Key.kern: -0.41, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
     func setupVisualElements() {
         self.addSubview(imageLogin)
+        self.addSubview(imageLabel)
 
         NSLayoutConstraint.activate([
 
@@ -35,6 +49,12 @@ class LoginView: UIView {
             imageLogin.heightAnchor.constraint(equalToConstant: 304.94),
             imageLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 47),
             imageLogin.topAnchor.constraint(equalTo: self.topAnchor, constant: 179),
+
+            
+            imageLabel.widthAnchor.constraint(equalToConstant: 265),
+            imageLabel.topAnchor.constraint(equalTo: imageLogin.bottomAnchor, constant: 8),
+            imageLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 63),
+            // imageLogin.trailingAnchor.constraint(equalTo: self.topAnchor, constant: -62),
 
         ])
     }
