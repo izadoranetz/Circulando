76afe91119a898349c193b879b234024718c4922
Izadora Netz <izadora.ns@gmail.com>
2022-11-28 16:40:46 -0300

    Tela Login, Controller, Coordinator, LoginView

---
+++ Circulando/Controller/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Circulando
+//
+//  Created by Izadora Netz on 28/11/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Circulando/Coordinator/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  Circulando
+//
+//  Created by Izadora Netz on 28/11/22.
+//
+
+import Foundation
+
+protocol Coordinator {
+
+    func start ()
+
+}
